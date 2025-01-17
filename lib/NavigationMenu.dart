0d995eecc0efe158b1b1421a2d169d87f3afc232
Ashish-JaiswalAJ <ashishaj262002@gmail.com>
2025-01-17 14:06:38 +0530

    new commit

--- lib/NavigationMenu.dart
+++ lib/NavigationMenu.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
 import 'HomeScreen.dart';
 import 'StatusScreen.dart';
-import 'CallsScreen.dart';
 import 'CommunitiesScreen.dart';
+import 'CallsScreen.dart';
 
-class NavigationMenu extends StatelessWidget {
+class NavigationMenu extends StatefulWidget {
   const NavigationMenu({super.key});
+
+  @override
+  _NavigationMenuState createState() => _NavigationMenuState();
+}
+
+class _NavigationMenuState extends State<NavigationMenu> {
+  int _selectedIndex = 0;
+
+  // List of screens for navigation
+  final List<Widget> _screens = [
+    HomeScreen(),
+    StatusScreen(),
+    CommunitiesScreen(),
+    CallsScreen(),
+  ];
+
+  void _onDestinationSelected(int index) {
+    setState(() {
+      _selectedIndex = index; // Update the selected index
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _screens[_selectedIndex], // Show the screen based on the selected index
       bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.chat), label: 'Home'),
-            NavigationDestination(icon: Icon(Icons.upcoming), label: 'Updates'),
-            NavigationDestination(icon: Icon(Icons.group), label: 'Group'),
-            NavigationDestination(icon: Icon(Icons.call), label: 'Calls'),
-          ],
+        selectedIndex: _selectedIndex, // Current selected index
+        onDestinationSelected: _onDestinationSelected, // Callback for selecting a tab
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.chat), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.upcoming), label: 'Updates'),
+          NavigationDestination(icon: Icon(Icons.group), label: 'Group'),
+          NavigationDestination(icon: Icon(Icons.call), label: 'Calls'),
+        ],
       ),
     );
   }
 }
+
+
